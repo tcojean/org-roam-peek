88ac65b3a5928b5e7b6dd13dc8c2d021d8e0cc68
Domse007 <dominikkeller55@gmail.com>
2023-04-14 22:33:24 +0200

    Fixed bugs resulting in incorrect sizing and not appearing posframe.

--- org-roam-peek.el
+++ org-roam-peek.el
@@ -51,15 +51,15 @@ alternatives"
   :group 'org-roam-peek
   :type 'number)
 
-(defcustom org-roam-peek-posframe-width nil
+(defcustom org-roam-peek-posframe-width (lambda () (/ (frame-width) 5))
   "The width of the posframe."
   :group 'org-roam-peek
-  :type 'number)
+  :type '(choice number function))
 
-(defcustom org-roam-peek-posframe-height nil
+(defcustom org-roam-peek-posframe-height (lambda () (/ (frame-height) 4))
   "The height of the posframe."
   :group 'org-roam-peek
-  :type 'number)
+  :type '(choice number function))
 
 (defcustom org-roam-peek-buffer-name "*org-roam-peek*"
   "Name of the buffer that is shown in the posframe."
@@ -81,16 +81,20 @@ alternatives"
 (defvar org-roam-peek--last-id ""
   "Id of the last visible id.")
 
+(defcustom org-roam-peek--link-types '("id" "roam")
+  "List of types on which org-roam-peek will acto on.")
+
 ;;; Functions:
-(defun org-roam-peek--pst-cmd ()
-  "Function that is called to see if the cursor is hovering over a link."
+(defun org-roam-peek--post-cmd ()
+  "Function that is called to see if the cursor is hovering over a link. If
+that is the case, show the preview."
   (let* ((object (org-element-context))
          (type (car object))
          (link-info (car (cdr object))))
     (when (and (eq type 'link) (not org-roam-peek--shown))
       (let ((link-type (plist-get link-info :type))
             (link-id (plist-get link-info :path)))
-        (when (string-equal link-type "id")
+        (when (member link-type org-roam-peek--link-types)
           (get-buffer-create org-roam-peek-buffer-name)
           (with-current-buffer org-roam-peek-buffer-name
 	    ;; must only be reloaded if the id has changed.
@@ -98,12 +102,18 @@ alternatives"
 	      (delete-region (point-min) (point-max))
 	      (insert-file-contents (car (org-roam-id-find link-id nil)))
 	      (let ((inhibit-message t))(org-mode))) ; supress minibuffer output
-            (org-roam-peek--show-posframe (buffer-name)))
+	    (org-roam-peek--show-posframe (buffer-name)))
 	  (setq org-roam-peek--shown t
 		org-roam-peek--last-id link-id))))
     (when (and (not (eq type 'link)) org-roam-peek--shown)
       (progn (posframe-hide org-roam-peek-buffer-name)
              (setq org-roam-peek--shown nil)))))
+
+(defun org-roam-peek--posframe-size (num-or-func)
+  "Get the height or width. NUM-OR-FUNC is either a number or function."
+  (cond ((functionp num-or-func) (funcall num-or-func))
+	((numberp num-or-func) num-or-func)
+	(t (error "Must be function or number."))))
 
 (defun org-roam-peek--show-posframe (buffer)
   "Show a posframe with the peeked BUFFER."
@@ -113,23 +123,25 @@ alternatives"
          :border-width org-roam-peek-posframe-border-width
          :border-color
          (face-attribute 'org-roam-peek-posframe-border :background nil t)
-         (list :width (or org-roam-peek-posframe-width
-                          (/ (window-width) 3))
-               :height (or org-roam-peek-posframe-height
-                           (/ (window-height) 3)))))
+         (list :width
+	       (org-roam-peek--posframe-size org-roam-peek-posframe-width)
+	       :height
+	       (org-roam-peek--posframe-size org-roam-peek-posframe-height))))
 
 ;;;###autoload
 (define-minor-mode org-roam-peek-mode
   "Toggle org-roam link peeking."
   :lighter " pk"
+  :global nil
   (if org-roam-peek--enabled
-      (progn (setq org-roam-peek--enabled nil)
-	     (remove-hook 'post-command-hook 'org-roam-peek--pst-cmd)
-             (kill-buffer org-roam-peek-buffer-name)
-             (setq org-roam-peek--shown nil))
+      (progn (remove-hook 'post-command-hook 'org-roam-peek--post-cmd)
+	     (kill-buffer org-roam-peek-buffer-name)
+	     (setq org-roam-peek--shown nil
+		   org-roam-peek--enabled nil
+		   org-roam-peek--last-id nil))
     (when (posframe-workable-p)
       (setq org-roam-peek--enabled t)
-      (add-hook 'post-command-hook 'org-roam-peek--pst-cmd))))
+      (add-hook 'post-command-hook 'org-roam-peek--post-cmd))))
 
 (provide 'org-roam-peek)
 
